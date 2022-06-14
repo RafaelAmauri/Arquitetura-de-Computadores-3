97a4d5efef203b12d5d3859300bdfcaf0939e837
Rafael <amauri.rafael33@gmail.com>
2022-06-14 00:21:14 -0300

    Remove test files

--- Simulador Tomasulo/src/back_test.py
+++ Simulador Tomasulo/src/back_test.py
@@ -2,7 +2,7 @@ import time
 import backend.back
 import os
 
-b = backend.back.Tomasulo(os.getcwd()+'/assets/instrucoes2.txt')
+b = backend.back.Tomasulo(os.getcwd()+'/assets/instrucoes1.txt')
 
 for i in range(100):
     status, unidades_funcionais, inst_queue = b.clock()
